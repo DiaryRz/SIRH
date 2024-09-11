fa68222ca3e2f4209a3f37b5855eb220c980a02d
Your Name <you@example.com>
2024-09-11 17:35:27 +0200

    Modif

---
+++ src/main/java/com/sirh/demo/controllers/TeletravailController.java
@@ -0,0 +1,26 @@
+package com.sirh.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
+
+import com.sirh.demo.models.Teletravail;
+import com.sirh.demo.services.TeleTravailService;
+import java.sql.Timestamp;
+
+@RestController
+@RequestMapping("/sirh/teletravails")
+public class TeletravailController {
+     @Autowired
+    private TeleTravailService TeleTravailService;
+
+    @GetMapping("/TTDuJour")
+    public List<Teletravail> listeDesPersonnesARecuperer(@RequestParam Timestamp date) throws Exception {
+        List<Teletravail> teletravail = TeleTravailService.teletravailsEntreDeuxDates(date);
+        return teletravail;
+    }
+    
+}
